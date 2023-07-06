883b7b3a91111abe217e1af18b5eb8af340c4a15
i.borentegonzalez <i.borentegonzalez@accenture.com>
2023-07-06 11:31:29 +0200

    adding some examples

---
+++ scripts/getPRText_WithArgs.sh
@@ -0,0 +1,24 @@
+inputtext=$1
+inputfile=$2
+#inputfile="prdesc.txt"
+#inputtext=$(cat prdesc.txt)
+echo "no test" > TestToBeRun.txt
+echo "$inputtext" | while IFS= read -r line
+do
+if [[ $OpenClassMD == 1 ]]
+then
+if [[ $line == *"\`\`\`"* ]]
+then
+	CloseMD=1
+	OpenClassMD=0
+else
+	ListOfClass=$line
+	echo "$line" > TestToBeRun.txt
+fi
+fi
+if [[ $line == *"\`\`\`TestsToBeRun"* ]]
+then
+	OpenClassMD=1
+fi	
+done
+cat TestToBeRun.txt
